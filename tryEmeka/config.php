48db15451185e910831ab17ddac05d20a034726f
Z-yler <79811736+Z-yler@users.noreply.github.com>
2024-08-28 21:12:39 +0100

    Update config.php

--- tryEmeka/config.php
+++ tryEmeka/config.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect('localhost','root','','try');
+$connect=mysqli_connect('bmhl4abwuiwhv1he13wl-mysql.services.clever-cloud.com','uqaa1ba2ran8gbgm','8GCojR5jTlVvxKT2Sr0G','bmhl4abwuiwhv1he13wl');
 
 
 session_start();
